d44250ad0a9aef0e9fc8f7515839aa198654c9e4
VitorParenti <vitorparenti351@gmail.com>
2024-12-20 20:37:40 -0300

    Testando github no NetBeans

--- SGBJava/src/main/java/br/cefet/inf/sgbjava/SGBJava.java
+++ SGBJava/src/main/java/br/cefet/inf/sgbjava/SGBJava.java
@@ -11,6 +11,6 @@ package br.cefet.inf.sgbjava;
 public class SGBJava {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!!!!");
     }
 }
