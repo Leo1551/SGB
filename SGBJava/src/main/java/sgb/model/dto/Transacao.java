ac82cadb744255e1c7786e8f38b22940c3a129fc
VitorParenti <vitorparenti351@gmail.com>
2025-01-01 14:44:17 -0300

    Revert "Implementando os metodos getMatricula, getValor e getData na  classe Transacao "
    This reverts commit adc047385237a2f2dab53f25b0b68185d946eedf.

--- SGBJava/src/main/java/sgb/model/dto/Transacao.java
+++ SGBJava/src/main/java/sgb/model/dto/Transacao.java
@@ -3,37 +3,25 @@ package sgb.model.dto;
 import java.util.Date;
 
 public class Transacao {
-
     private long matricula;
     /*O valor da transação é guardado em centavos, por isso int. Isso é feito 
     para evitar pontos flutuantes, os erros em suas operações e gasto de memória*/
     private int valor;
     private java.util.Date data;
-
-    public Transacao(long matricula, int valor) {
-        this.matricula = matricula;
-        this.valor = valor;
-        this.data = new java.util.Date();
-    }
-
-    public long getMatricula() {
-        return matricula;
-    }
-
-    public long getValor() {
-        return valor;
-    }
-
-    public java.util.Date getData(){
-        return data;
+    
+    public Transacao (long matricula, int valor){
+        this.matricula=matricula;
+        this.valor=valor;
+        this.data= new java.util.Date();
     }
     
-    public String toString() {
-        String str = "matricula: " + matricula + "\nvalor: " + valor + "\ndata: " + data + "\n";
+    
+    public String toString(){
+        String str="matricula: "+matricula+"\nvalor: "+valor+"\ndata: "+data+"\n";
         return str;
     }
-
-    public static void main(String[] args) {
+    
+    public static void main(String[] args){
         System.out.println(new Transacao(31232132213321L, 32));
     }
 }
