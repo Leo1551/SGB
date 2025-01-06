f1638c26bddc0370552314c5278ae70435234412
aluno <aluno@5A445JW93>
2025-01-06 10:03:40 -0300

    envio do CDU 001A incompleto

---
+++ SGBJava/src/main/java/br/cefet/inf/sgbjava/SaldoBD.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.cefet.inf.sgbjava;
+import java.sql.*;
+
+/**
+ *
+ * @author Bruno
+ */
+public class SaldoBD {
+    private static String url = "jdbc:mysql://localhost:3306/saldo";
+    private static String user = "root";
+    private static String senha = "";
+    
+    private static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url, user, senha);
+    }
+    
+}
