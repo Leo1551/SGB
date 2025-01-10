5f727dc298845c4e30dea44c7472c2d2de4f77c3
BrunoSilva525 <brunomaisbh525@gmail.com>
2025-01-10 06:48:25 -0300

    Criação da pasta de controladores(Servlets)

--- SGBWeb/src/main/java/sgb/model/controllers/SaldoServlet.java
+++ SGBWeb/src/main/java/sgb/model/controllers/SaldoServlet.java
@@ -8,37 +8,50 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.sql.*;
+import java.util.*;
 
 /**
  *
  * @author usuario
  */
-@WebServlet("/getSaldo")
+@WebServlet(urlPatterns = {"/SaldoServlet", "/main"})
 public class SaldoServlet extends HttpServlet {
-    private SaldoDAO saldoDAO;
-
-    @Override
-    public void init() {
-        saldoDAO = new SaldoDAO();
-    }
+    private static final long serialVersionUID = 1L;
+    private String url = "jdbc:mysql://localhost:3306/sgb";
+    private String usuario = "root";
+    private String senha = "";
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Connection conexao = null;
+        Statement stmt = null;
+        List<Saldo> saldos = new ArrayList<>();
         try {
-            Long idUsuario = Long.valueOf(request.getParameter("id"));
-            Saldo saldo = saldoDAO.getSaldoId(idUsuario);
-            if (saldo != null) {
-                request.setAttribute("saldo", saldo);
-            } else {
-                request.setAttribute("errorMessage", "Nenhum saldo encontrado para o ID fornecido.");
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conexao = DriverManager.getConnection(url, usuario, senha);
+            stmt = conexao.createStatement(); // Inicializando o Statement
+            String sql = "SELECT id, saldo FROM cadastros";
+            ResultSet rs = stmt.executeQuery(sql);
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                int saldo = rs.getInt("saldo");
+                saldos.add(new Saldo(id, saldo));
             }
-            request.getRequestDispatcher("teste.jsp").forward(request, response);
-        } catch (NumberFormatException e) {
-            request.setAttribute("errorMessage", "ID inválido.");
-            request.getRequestDispatcher("error.jsp").forward(request, response);
- } catch (Exception e) {
+            request.setAttribute("saldos", saldos);
+            request.getRequestDispatcher("/core/testeSaldo.jsp").forward(request, response);
+            //request.getRequestDispatcher("../testeSaldo.jsp").forward(request, response);
+        } catch (Exception e) {
             e.printStackTrace();
             request.setAttribute("errorMessage", "Ocorreu um erro ao buscar o saldo.");
-            request.getRequestDispatcher("error.jsp").forward(request, response);
+            request.getRequestDispatcher("../core/erro.jsp").forward(request, response);
+        } finally {
+            // Fechar recursos
+            try {
+                if (stmt != null) stmt.close();
+                if (conexao != null) conexao.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
