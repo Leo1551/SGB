4b65d08f197a27e7d35effd6f2dc937ec539a52c
VitorParenti <vitorparenti351@gmail.com>
2025-01-11 23:24:31 -0300

    CDU 010-Criando servlet de historico

---
+++ SGBWeb/src/main/java/sgb/model/controllers/HistoricoServlet.java
@@ -0,0 +1,72 @@
+package sgb.model.controllers;
+
+import java.util.Date;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.*;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import sgb.model.dao.TransacaoDAO;
+import sgb.model.dto.Transacao;
+
+@WebServlet("/historico")
+public class HistoricoServlet extends HttpServlet {
+
+    private String converteTransacaoParaJSON(Transacao tr) {
+        DecimalFormat formatacaoDinheiro = new DecimalFormat("#.##");
+        
+        StringBuilder rJson = new StringBuilder();
+        rJson.append("{");
+        rJson.append("\"tipo\":");
+        if (tr.getValor() > 0) {
+            rJson.append("\"recarga\",");
+        } else {
+            rJson.append("\"compra\",");
+        }
+        rJson.append("\"data\":");
+        rJson.append("\"");
+        rJson.append(new SimpleDateFormat("MM-dd-yyyy").format(tr.getData()));
+        rJson.append("\",");
+        rJson.append("\"hora\":");
+        rJson.append(new SimpleDateFormat("HH:mm").format(tr.getData()));
+        rJson.append("\",");
+        rJson.append("\"funcionario\":");
+        rJson.append("\"");
+        rJson.append(tr.getFuncionario());
+        rJson.append("\",");
+        rJson.append("\"valor\":");
+        rJson.append("\"");
+        //o valor é guardado como int, com o número de centavos
+        rJson.append(formatacaoDinheiro.format(tr.getValor()/100));
+        rJson.append("\",");
+        rJson.append("\"saldoAtual\":");
+        rJson.append("\"");
+        //o valor é guardado como int, com o número de centavos
+        rJson.append(formatacaoDinheiro.format(tr.getSaldo()/100));
+        rJson.append("\"");
+                rJson.append("}");
+
+        
+        return rJson.toString();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        long matricula = Long.parseLong(request.getParameter("matricula"));
+        TransacaoDAO trDAO = new TransacaoDAO();
+        Transacao[] transacoes=null;
+        try {
+            transacoes = trDAO.getTransacoes(20225678908L);
+        } catch (SQLException | ClassNotFoundException ex) {
+            Logger.getLogger(HistoricoServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+       request.setAttribute("test", converteTransacaoParaJSON(transacoes[0]));
+       request.getRequestDispatcher("/core/historico/historico.jsp").forward(request, response);
+    }
+}
