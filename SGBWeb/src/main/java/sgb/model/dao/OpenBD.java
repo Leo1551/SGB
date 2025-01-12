6a7b481ac5a9697a8ea28a018c76879ce2665810
VitorParenti <vitorparenti351@gmail.com>
2025-01-11 23:38:03 -0300

    Adicionando método getConnectionComDriver()

--- SGBWeb/src/main/java/sgb/model/dao/OpenBD.java
+++ SGBWeb/src/main/java/sgb/model/dao/OpenBD.java
@@ -13,11 +13,17 @@ import java.sql.SQLException;
  * @author aluno
  */
 public class OpenBD {
+    private final String driver = "com.mysql.cj.jdbc.Driver";
     private static final String url = "jdbc:mysql://localhost:3306/sgb";
     private static final String usuario = "root";
     private static final String senha = "";
 
     public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url, usuario, senha);
+    }
+    
+    public  Connection getConnectionComDriver() throws SQLException, ClassNotFoundException {
+        Class.forName(driver);
         return DriverManager.getConnection(url, usuario, senha);
     }
 }
