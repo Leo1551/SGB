4210a2fe22e78d092194d1153f232cf7cdea33d6
BrunoSilva525 <brunomaisbh525@gmail.com>
2025-01-12 21:01:57 -0300

    CDU 001 - ajustes necessários para o funcionamento do DAO de login.

--- SGBWeb/src/main/java/sgb/model/dao/LoginDAO.java
+++ SGBWeb/src/main/java/sgb/model/dao/LoginDAO.java
@@ -9,32 +9,34 @@ import sgb.model.dto.Login;
 
 
 
-
-
 public class LoginDAO {
     
     
     public static boolean verificacaoLogin(Login login){
-        
+        ConectarDAO con = new ConectarDAO();
+        Connection conexao = null;
         try{
-           Connection conexao = OpenBD.getConnection();
+           conexao = con.conectar();
+           if (conexao == null) {
+                throw new SQLException("Falha ao conectar ao banco de dados.");
+           }
            PreparedStatement caixa = conexao.prepareStatement("SELECT EXISTS(SELECT 1 FROM cadastros WHERE matricula = ? AND senha = ?)");
            //abre suporte em lote
            caixa.setString(1, login.getMatricula());
            caixa.setString(2, login.getSenha());
 
            ResultSet resultado = caixa.executeQuery();
-           
-            if (resultado.next())
-                return  resultado.getBoolean(1);
+           Boolean existe = false;
+           if(resultado.next())
+               existe = resultado.getBoolean(1);
+           return  existe;
              
         } catch (SQLException e) {
-            
-            
+
             System.err.println("Erro ao consultar login no banco de dados \n\n\n" + e.getMessage());
             return false;
         }
-        return false;
+
       
     
     }
